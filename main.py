496b9d6644f713d64c092a4e794865b4898218c3
Shashank <shashankg.2k@gmail.com>
2023-07-28 16:21:14 +0530

    update main

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 try:
-    from utils import set_seed, model_summary
-    from utils.experiment import Experiment
-    from models.resnet import ResNet18
-    from Dataset import CIFAR10
+    from core.utils import set_seed, model_summary
+    from core.utils.experiment import Experiment
+    from core.models.resnet import ResNet18
+    from core.Dataset import CIFAR10
 except ModuleNotFoundError:
     from utils.get_device import set_seed, model_summary
     from utils.experiment import Experiment
     from models import *
-    from Dataset import *
+    from Dataset import * 
 
 
 set_seed(42)
@@ -21,12 +21,12 @@ def print_summary(model_):
     print(model_summary(model_, input_size=(batch_size, 3, 32, 32)))
 
 
-def create_experiment(model_, dataset, criterion, epochs, scheduler):
-    return Experiment(model_, dataset, criterion=criterion, epochs=epochs, scheduler=scheduler)
+def create_experiment(model_, dataset, criterion, epochs, scheduler, optimizer):
+    return Experiment(model_, dataset, criterion=criterion, epochs=epochs, scheduler=scheduler, optimizer=optimizer)
 
 
-def main(criterion='crossentropy', epochs=20, scheduler='one_cycle'):
-    experiment = create_experiment(criterion=criterion, epochs=epochs, scheduler=scheduler)
+def main(criterion='crossentropy', epochs=20, scheduler='one_cycle', optimizer = 'SGD'):
+    experiment = create_experiment(criterion=criterion, epochs=epochs, scheduler=scheduler, optimizer=optimizer)
     experiment.execute()
     experiment.train.plot_stats()
     experiment.test.plot_stats()
